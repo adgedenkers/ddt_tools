4c4b90f1eb05056586d7c20f9b0c67fc72f7ff0f
Adge Denkers <adriaan.denkers@va.gov>
2023-10-27 09:51:30 -0400

    updated cleaned_df logic

--- fz.py
+++ fz.py
@@ -9,6 +9,7 @@ def clean_dataframe(df):
 
     # loop through each row and column
     for col in df.columns:
+        # cleaned df column = (if float, convert to int, then convert everything to string except nulls)
         cleaned_df[col] = df[col].apply(lambda x: str(int(x)) if isinstance(x, float) and x.is_integer() else (str(x)).strip() if pd.notnull(x) else x)
 
     return cleaned_df
