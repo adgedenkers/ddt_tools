db84d089977d9fa038799ccf21b420b3dbf3f6d6
Adge Denkers <adriaan.denkers@va.gov>
2023-10-27 09:49:53 -0400

    added fz.py

---
+++ fz.py
@@ -0,0 +1,15 @@
+
+
+
+import pandas as pd
+
+def clean_dataframe(df):
+    # create a new dataframe to store the cleaned data
+    cleaned_df = pd.DataFrame()
+
+    # loop through each row and column
+    for col in df.columns:
+        cleaned_df[col] = df[col].apply(lambda x: str(int(x)) if isinstance(x, float) and x.is_integer() else (str(x)).strip() if pd.notnull(x) else x)
+
+    return cleaned_df
+        
